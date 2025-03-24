f30ea20ec079a7541ccb6f263aae1135888f0b1b
roberto cogolludo jimenez <roberto.cogolludo@educa.madrid.org>
2025-03-24 17:01:51 +0100

    clase del lunes

--- Funciones.sql
+++ Funciones.sql
@@ -23,7 +23,7 @@ select contar_categorias("Music");
 
 select name from category;
 
--- si alquila mas de 50 pelis tienes descuento de 15% (-5 por tier menos) pasan cosas y si alquilan mas de 20 tal y mas de 10 cual.
+-- si alquila mas de 50 pelis tienes descuento de 15% (-5 por tier menos), si alquilan mas de 20 tal y mas de 10 cual.
 
 DELIMITER $$
 CREATE FUNCTION aplicar_descuento(cliente int)
@@ -78,3 +78,43 @@ END
 $$
 
 select sum100();
+
+-- Crear el Trigger after_payment_insert (7pts):
+
+-- Crea una tabla que contenga como columnas el staff_id y un staff_revenue.
+
+-- Crea un trigger llamado after_payment_insert que se active después de cada inserción en la tabla payment.
+-- El trigger debe actualizar la tabla staff_revenue_table de la siguiente manera:
+-- Si no existe un registro para el staff_id en staff_revenue, se debe insertar uno nuevo con el amount del nuevo pago.
+-- Si existe un registro, se debe actualizar el staff_revenue sumando el amount del nuevo pago.
+
+create table staff_table (
+staff_id tinyint foreign key,
+staff_revenue double
+);
+
+create trigger after_payment_insert
+after insert
+for each row
+begin
+declare total_revenue decimal;
+
+-- create error handler 
+create error handler()
+begin
+declare continue handler for
+sqlexception
+begin 
+select'ocurrio un error'as message rollback 
+end
+declare continue handler for sql warnings
+begin
+select 'espabila manin' as message
+end
+start transaction;
+insert into actor(actor_id, first_name, last_name, lat_update) values
+(1,'roberto','cogolludo',now());
+insert into actor(actor_id, first_name, last_name, lat_update) values
+('laura','delgado',now());
+commit
+end
